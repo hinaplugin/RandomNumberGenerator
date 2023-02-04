f4da558ff8207fba3950f6d6caa6ad58f00e812f
hina2113 <110330118+hinaplugin@users.noreply.github.com>
2023-02-05 03:24:39 +0900

    Update Commands.java

--- src/main/java/com/hinaplugin/randomnumbergenerator/randomnumbergenerator/Commands.java
+++ src/main/java/com/hinaplugin/randomnumbergenerator/randomnumbergenerator/Commands.java
@@ -7,24 +7,28 @@ import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Random;
+import java.util.regex.Pattern;
 
 public class Commands implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length == 3){
-            if (isNumeric(args[0]) || isNumeric(args[1])){
+            if (isNumber(args[0]) || isNumber(args[1]) || isNumeric(args[2])){
                 sender.sendMessage(ChatColor.RED + "usage: /rng <range1> <range2> <count>");
+                sender.sendMessage(ChatColor.RED + "ただし，range及びcountに使えるのは自然数のみです．");
                 return true;
             }
 
-            int range1 = Integer.parseInt(args[0]);
-            int range2 = Integer.parseInt(args[1]);
-            int count = Integer.parseInt(args[2]);
+            final int range1 = Integer.parseInt(args[0]);
+            final int range2 = Integer.parseInt(args[1]);
+            final int count = Integer.parseInt(args[2]);
             final StringBuilder builder = new StringBuilder();
+            int sum = 0;
             if (range1 <= range2){
                 for (int i = 0; i < count; i++){
                     final Random random = new Random();
                     final int answer = random.nextInt((range2 - range1) + 1) + range1;
+                    sum += answer;
                     builder.append(answer);
                     if (i != count - 1){
                         builder.append(", ");
@@ -35,6 +39,7 @@ public class Commands implements CommandExecutor {
                 for (int i = 0; i < count; i++){
                     final Random random = new Random();
                     final int answer = random.nextInt((range1 - range2) + 1) + range2;
+                    sum += answer;
                     builder.append(answer);
                     if (i == count - 1){
                         builder.append(", ");
@@ -42,6 +47,7 @@ public class Commands implements CommandExecutor {
                 }
                 sender.sendMessage(ChatColor.GREEN + String.valueOf(range2) + "から" + range1 + "までの範囲で" + count + "個生成しました．");
             }
+            builder.append(" (合計: ").append(sum).append(")");
             sender.sendMessage(builder.toString());
             return true;
         }
@@ -51,5 +57,10 @@ public class Commands implements CommandExecutor {
 
     private boolean isNumeric(String number){
         return !number.chars().allMatch(Character::isDigit);
+    }
+
+    private boolean isNumber(String number){
+        Pattern pattern = Pattern.compile("^\\d+$|-\\d+$");
+        return !pattern.matcher(number).matches();
     }
 }
